8d644e65fa4addd83a4fbb80361aa9d9cb6395a9
Stephen Hibbs <steve@alpinelaboratories.com>
2017-07-08 15:42:30 -0600

    draws our grid and sets a pixel to red!

--- panelVisualizer.py
+++ panelVisualizer.py
@@ -1,15 +1,36 @@
 
 from graphics import * #import the graphics
 from classes import *
+class PanelVisualizer():
+    canvasWidth = 500
+    canvasHeight = 500
+    pixelDistance = 10 #this is how much distance we put between pixels
+    # canvasPixels
+    numColumns = 0
+    numRows = 0
+    win = GraphWin("My Circle", canvasWidth, canvasHeight)
+    def __init__(self, number_of_columns, number_of_rows):
+        self.numColumns = number_of_columns
+        self.numRows = number_of_rows
+        self.pixelDistance = self.canvasWidth / (self.numColumns+1)
+        self.canvasHeight = self.pixelDistance*(self.numRows+1)
+        self.canvasPixels = [Circle(Point(0,0),10) for n in range(self.numRows*self.numColumns) ]
+        for i in range(self.numRows):
+            for j in range(self.numColumns):
+                self.canvasPixels[i*self.numColumns + j].move((i+.5)*self.pixelDistance, (j+.5)*self.pixelDistance)
+                self.canvasPixels[i*self.numColumns + j].draw(self.win)
 
+
+    def set_pixel(self, row, col, pixel):
+        pixel.print_colors()
+        self.canvasPixels[row * self.numColumns + col * self.numRows].setFill('red')
+    def wait_for_exit(self):
+        self.win.getMouse() # Pause to view result
+        self.win.close()    # Close window when done
 def main():
-    win = GraphWin("My Circle", 100, 100)
-    c = Circle(Point(50,50), 10)
-    c.setFill('red');
-    c.draw(win)
-    win.getMouse() # Pause to view result
-    win.close()    # Close window when done
+    panel = PanelVisualizer(5, 6)
+    pix = Pixel(10, 40, 50)
+    panel.set_pixel(1,2, pix)
+    panel.wait_for_exit()
 
 main()
-
-#def drawPixel(row, col, pixel):
