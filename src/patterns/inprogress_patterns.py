b8d5f80342763f2762bd94312e5fa6dbbd8a7ed2
dgoleOnPi <dgole100@gmail.com>
2017-11-15 17:36:52 +0000

    added fps print out

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -20,12 +20,13 @@ class AudioReactiveTestPattern(PanelPattern):
     def __init__(self, m, n):
         PanelPattern.__init__(self, m, n)
         self.call_name = 'arTest';
+        self.frameCount = 0
         self.frame_sleep_time = 0.0
         self.pix_np = np.zeros([3,self.m,self.n])
-        self.stream = micStream.Stream(fps=24,nBuffers=4)
-	self.t0 = time.time()
-	selt.t1 = time.time()
-	print(self.stream.freqs[0:10])
+        self.stream = micStream.Stream(fps=60,nBuffers=6)
+	self.t0 = time.time() 
+        self.t1 = time.time()
+        print(self.stream.freqs[0:10])
         print(self.stream.freqs[-10:])
     def update_pixel_arr(self):
         # update and change the pixel array
@@ -34,6 +35,8 @@ class AudioReactiveTestPattern(PanelPattern):
             #print(np.mean(self.stream.noteSpectrum))
             self.pix_np[0,0,:] = self.stream.noteSpectrum
             self.pixel_arr = [ [Pixel(self.pix_np[0,j,i],self.pix_np[1,j,i],self.pix_np[2,j,i]) for i in range(self.n) ] for j in range(self.m) ]
-	self.t1 = time.time()
-	print(self.t1 - self.t0)
-	self.t0 = self.t1
+            self.frameCount+=1
+            if self.frameCount%100==0:
+	        self.t1 = time.time()
+	        print('fps last 100 frames: ' + str(100 / (self.t1 - self.t0)))
+                self.t0 = self.t1
