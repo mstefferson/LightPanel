5b9e97238f3c026893ad48aac5feb67b904456f8
dgole <dgole100@gmail.com>
2017-11-15 00:34:45 -0700

    Update inprogress_patterns.py

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -15,14 +15,13 @@ class AudioReactiveTestPattern(PanelPattern):
         self.call_name = 'arTest';
         self.frame_sleep_time = 0.0
         self.pix_np = np.zeros([3,m,n])
-        #self.stream1 = microphone.Stream()
-        #print(dir(self.stream1))
+        self.stream1 = microphone.Stream()
+        print(dir(self.stream1))
     def update_pixel_arr(self):
-        global stream
         # update and change the pixel array
         print(dir(self.stream1))
-        success = stream.readAndCalc()
+        success = stream1.readAndCalc()
         if success:
-            print(np.mean(stream.noteSpectrum))
-            self.pix_np[0,0,:] = stream.noteSpectrum
+            print(np.mean(stream1.noteSpectrum))
+            self.pix_np[0,0,:] = stream1.noteSpectrum
             self.pixel_arr = [ [Pixel(self.pix_np[0,m,n],self.pix_np[1,m,n],self.pix_np[2,m,n]) for i in range(self.n) ] for j in range(self.m) ]
