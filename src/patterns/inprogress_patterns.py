8d73bcd73b1a35c535ee2658ef1eff8a82fc2c0b
dgole <dgole100@gmail.com>
2017-11-14 20:03:01 -0700

    Update inprogress_patterns.py

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -13,3 +13,13 @@ class RandwalkPattern(PanelPattern):
         PanelPattern.__init__( self, m, n )
         self.call_name = 'randwalk';
 
+# audioReactive test pattern
+class AudioReactiveTestPattern(PanelPattern):
+    def __init__(self, m, n):
+        PanelPattern.__init__(self, m, n)
+        self.call_name = 'arTest';
+        self.frame_sleep_time = 0.0
+    def update_pixel_arr(self):
+        # update and change the pixel array
+        self.pixel_arr = [ [Pixel(30,0,0) for i in range( self.n ) ] for j in range(self.m) ]
+        
