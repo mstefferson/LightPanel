70fe667e4369a6f13f3427a9f56c9fcbca56ab8f
dgole <dgole100@gmail.com>
2017-11-15 13:35:16 -0700

    Update music.py

--- src/audioReactive/music.py
+++ src/audioReactive/music.py
@@ -7,11 +7,9 @@ from numpy import *
 from scipy.ndimage.filters import gaussian_filter1d
 import config
 
-
-
-
 class ExpFilter:
-    """Simple exponential smoothing filter"""
+    """Temporal exponential smoothing filter
+    """
     def __init__(self, val=0.0, alpha_decay=0.5, alpha_rise=0.5):
         """Small rise / decay factors = more smoothing"""
         self.alpha_decay = alpha_decay
@@ -25,7 +23,61 @@ class ExpFilter:
         else:
             alpha = self.alpha_rise if value > self.value else self.alpha_decay
         self.value = alpha * value + (1.0 - alpha) * self.value
-        return self.value
+            
+            
+            
+def getNotesToKeyMatrix(noteList, keyPattern=[0,2,4,5,7,9,11]):
+    matrix = np.zeros([12, len(noteList)])
+    for i in range(12):
+        for note in noteList:
+            scaleDegree = ((note-i%12)%12)-1
+            if scaleDegree in keyPattern:
+                matrix[i,j] = 1.0
+            else:
+                matrix[i,j] = 0.0
+    return matrix    
+    
+    
+    
+class Key:
+    def __init__(self, matrix, alpha=0.01):
+        self.keySums = ExpFilter(np.ones(12), alpha_rise=alpha, alpha_decay=alpha)
+        self.matrix = matrix
+        self.keyStringList = ['c', 'cs', 'd', 'ef', 'e', 'f', 'fs', 'g', 'af', 'a', 'bf', 'b' ]
+        self.currentKeyNum = 0
+    def update(self, newNoteSpectrum):
+        newKeySums = np.dot(self.matrix, newNoteSpectrum)
+        self.keySums.update(newKeySums)
+        self.currentKeyNum = np.argmax(self.keySums.value)
+    def printKey(self):
+        print("most likely key is " + self.keyStringList[self.currentKeyNum])
+        print(self.keySums.value)
         
+        
+        
+class Chord:
+    def __init__(self, noteList, alpha=0.1):
+        # define the 7 x notes matrix for each of 12 possible keys.  
+        chordRefMatrix = np.array([[0,4,7], [2,5,9], [4,7,11], [5,9,11], [7,11,2], [9,0,4], [11,2,5]])
+        self.chordMatrixList = []
+        for i in range(12):
+            self.chordMatrixList.append(np.zeros([7,len(noteList)]))
+        for keyNum in range(12):
+            for chordNum in range(7):
+                for note in noteList:
+                    if (note-keyNum%12)%12 -1 in chordRefMatrix[chordNum]:
+                        self.chordMatrixList[keyNum][chordNum, pixelNum] = 1.0
+                    else:
+                        self.chordMatrixList[keyNum][chordNum, pixelNum] = 0.0
+            self.chordSums = ExpFilter(np.zeros(7), alpha_rise=alpha, alpha_decay=alpha)
+            self.chordStringList = ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii']
+            self.currentChordNum = 0
+    def update(self, newNoteSpectrum, currentKeyNum):
+        newChordSums = np.dot(self.chordMatrixList[currentKeyNum], newNoteSpectrum)
+        self.chordSums.update(newChordSums)
+        self.currendChordNum = self.chordSums.argmax()
+    def printChord(self):
+        print("most likely chord is " + self.chordStringList[self.currentChordNum])
+        #print(self.chordSums)
         
         
