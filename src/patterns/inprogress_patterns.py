2174c9ab624f418e138813a587b8f41981ba8ccc
dgoleOnPi <dgole100@gmail.com>
2017-11-15 17:15:48 +0000

    fixed attribute error

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -5,9 +5,8 @@ from classes import Pixel
 import numpy as np
 from functioning_patterns  import PanelPattern
 sys.path.insert(0, '../audioReactive/')
-import microphone
+import micStream
 #import music
-
 
 # audioReactive test pattern
 class AudioReactiveTestPattern(PanelPattern):
@@ -16,13 +15,13 @@ class AudioReactiveTestPattern(PanelPattern):
         self.call_name = 'arTest';
         self.frame_sleep_time = 0.0
         self.pix_np = np.zeros([3,self.m,self.n])
-        self.stream1 = microphone.Stream()
-        print(dir(self.stream1))
+        self.stream = micStream.Stream(fps=24,nBuffers=4)
+	print(self.stream.freqs[0:10])
+        print(self.stream.freqs[-10:])
     def update_pixel_arr(self):
         # update and change the pixel array
-        #print(dir(self.stream1))
-        success = self.stream1.readAndCalc()
+        success = self.stream.readAndCalc()
         if success:
-            print(np.mean(self.stream1.noteSpectrum))
-            self.pix_np[0,0,:] = self.stream1.noteSpectrum
+            print(np.mean(self.stream.noteSpectrum))
+            self.pix_np[0,0,:] = self.stream.noteSpectrum
             self.pixel_arr = [ [Pixel(self.pix_np[0,j,i],self.pix_np[1,j,i],self.pix_np[2,j,i]) for i in range(self.n) ] for j in range(self.m) ]
