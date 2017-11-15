61de1d1865e2325962546444fec979693c0f2460
dgole <dgole100@gmail.com>
2017-11-15 13:57:28 -0700

    Update inprogress_patterns.py

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -7,7 +7,7 @@ import time
 from functioning_patterns  import PanelPattern
 sys.path.insert(0, '../audioReactive/')
 import micStream
-#import music
+import music
 
 # randwalk pattern
 class RandwalkPattern(PanelPattern):
@@ -24,10 +24,10 @@ class AudioReactiveTestPattern(PanelPattern):
         self.frame_sleep_time = 0.0
         self.pix_np = np.zeros([3,self.m,self.n])
         self.stream = micStream.Stream(fps=60,nBuffers=6)
-	self.t0 = time.time() 
+        self.t0 = time.time() 
         self.t1 = time.time()
-        print(self.stream.freqs[0:10])
-        print(self.stream.freqs[-10:])
+        #print(self.stream.freqs[0:10])
+        #print(self.stream.freqs[-10:])
     def update_pixel_arr(self):
         # update and change the pixel array
         success = self.stream.readAndCalc()
@@ -39,4 +39,29 @@ class AudioReactiveTestPattern(PanelPattern):
             if self.frameCount%100==0:
 	        self.t1 = time.time()
 	        print('fps last 100 frames: ' + str(100 / (self.t1 - self.t0)))
-                self.t0 = self.t1
+            self.t0 = self.t1
+		
+		
+# audioReactive test pattern
+class AudioReactiveChordPattern(PanelPattern):
+    def __init__(self, m, n):
+        PanelPattern.__init__(self, m, n)
+        self.call_name = 'arChord';
+        self.frameCount = 0
+        self.frame_sleep_time = 0.0
+        self.pix_np = np.zeros([3,self.m,self.n])
+        self.stream = micStream.Stream(fps=60,nBuffers=6)
+        self.volume = music.ExpFilter(0.0, alpha_rise=0.8, alpha_decay=0.3)
+        self.keyObj = music.Key(music.getNotesToKeyMatrix(self.stream.notes))
+        self.chordObj = music.Chord(stream.notes)
+    def update_pixel_arr(self):
+        # update and change the pixel array
+        success = self.stream.readAndCalc()
+        if success:
+	        self.volume.update(np.mean(self.stream.noteSpectrum))
+	        self.keyObj.update(self.stream.noteSpectrum)
+	        self.chordObj.update(self.stream.noteSpectrum, keyObj.currentKeyNum)
+            self.pix_np[0,0,:] = 100.0 * (self.stream.noteSpectrum / self.volume.value)
+            self.pixel_arr = [ [Pixel(self.pix_np[0,j,i],self.pix_np[1,j,i],self.pix_np[2,j,i]) for i in range(self.n) ] for j in range(self.m) ]
+            self.frameCount+=1
+ 
