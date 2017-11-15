22c3069a36141ecc1ac8580c78582a91a5e4c2b3
dgole <dgole100@gmail.com>
2017-11-15 10:20:31 -0700

    Update inprogress_patterns.py

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -3,6 +3,7 @@
 import sys
 from classes import Pixel
 import numpy as np
+import time
 from functioning_patterns  import PanelPattern
 sys.path.insert(0, '../audioReactive/')
 import micStream
@@ -22,12 +23,16 @@ class AudioReactiveTestPattern(PanelPattern):
         self.frame_sleep_time = 0.0
         self.pix_np = np.zeros([3,self.m,self.n])
         self.stream = micStream.Stream(fps=24,nBuffers=4)
+	self.t0 = time.time()
 	print(self.stream.freqs[0:10])
         print(self.stream.freqs[-10:])
     def update_pixel_arr(self):
         # update and change the pixel array
         success = self.stream.readAndCalc()
         if success:
-            print(np.mean(self.stream.noteSpectrum))
+            #print(np.mean(self.stream.noteSpectrum))
             self.pix_np[0,0,:] = self.stream.noteSpectrum
             self.pixel_arr = [ [Pixel(self.pix_np[0,j,i],self.pix_np[1,j,i],self.pix_np[2,j,i]) for i in range(self.n) ] for j in range(self.m) ]
+	dt = time.time() - t0
+	t0 = time.time
+	print(1./dt)
