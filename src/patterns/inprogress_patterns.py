38140aba95269a83de3ca505e9f4a7f1bcab0a82
dgole <dgole100@gmail.com>
2017-11-16 13:53:27 -0700

    Update inprogress_patterns.py

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -62,7 +62,7 @@ class AudioReactiveTheoryDemo(PanelPattern):
         if success:
 	    self.volume.update(np.mean(self.stream.noteSpectrum))
 	    self.keyObj.update(self.stream.noteSpectrum)
-            self.keyObj2.update(self.stream.noteSpectrum)
+            self.noteSumsObj.update(self.stream.noteSpectrum)
 	    self.chordObj.update(self.stream.noteSpectrum, self.keyObj.currentKeyNum)
             if self.frameCount%10==0:
                 print(self.volume.value)
