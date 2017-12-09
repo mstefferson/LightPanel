7810ee580578552910da79fa2cc42a3b06f932ff
dgole <dgole100@gmail.com>
2017-12-09 11:23:51 -0700

    Create patternHelpers.py

---
+++ src/patterns/patternHelpers.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+from __future__ import division
+import time
+import sys
+import numpy as np
+from numpy import *
+
+
+
+class Runner:
+    def __init__(self, n, speed, color, startLoc):
+        self.n = n
+        self.speed = speed
+        self.color = color
+        self.locInt = startLoc
+        self.locFloat = float(self.locInt)
+        self.outArray = np.zeros(local_N_PIXELS)
+        self.outZeros = np.zeros_like(self.outArray)
+        for i in range(self.locInt-self.n, self.locInt+self.n+1):
+            if i == self.locInt:
+                self.outArray[i] = 1.0
+            else:
+                self.outArray[i] = np.power(1.0 - (np.abs(self.locInt-i)/n) + 0.1,2)
+    def update(self):
+        self.locFloat = self.locFloat + self.speed
+        if int(self.locFloat) != self.locInt:
+            self.locInt = int(self.locFloat)
+            self.outArray = np.roll(self.outArray, int(np.sign(self.speed)))
+        if self.locInt == 0 or self.locInt == local_N_PIXELS-1:
+            self.speed = -self.speed            
+    def getFullOutArray(self):
+        if self.color=='r':
+            r = self.outArray
+            g = self.outZeros
+            b = self.outZeros
+        elif self.color=='g':
+            r = self.outZeros
+            g = self.outArray
+            b = self.outZeros
+        elif self.color=='b':
+            r = self.outZeros
+            g = self.outZeros
+            b = self.outArray
+        elif self.color=='p':
+            r = self.outArray
+            g = self.outZeros
+            b = self.outArray
+	returnArray = np.array([r,g,b])*255
+        return returnArray.astype(int)
