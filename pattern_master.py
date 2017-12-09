deea665462ffbc7c42ffa6fe1420aa842f633808
danOnPi <dgole100@gmail.com>
2017-12-09 17:55:15 +0000

    changed brightness

--- pattern_master.py
+++ pattern_master.py
@@ -11,7 +11,7 @@ import time
 
 
 # LED strip configuration:
-LED_COUNT      = 60      # Number of LED pixels.
+LED_COUNT      = 110      # Number of LED pixels.
 #LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_PIN        = 12      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
