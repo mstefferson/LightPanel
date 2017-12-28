981e8d0476bc8c7ca90cb62284676003277b6bf3
Michael Stefferson <mstefferson@gmail.com>
2017-12-28 11:47:55 -0700

    Cleaned up pattern master

--- pattern_master.py
+++ pattern_master.py
@@ -8,28 +8,19 @@ from classes import Panel
 import time
 import json
 
-
-# LED strip configuration:
-LED_COUNT      = 30      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-
-# panel configuration
-NUM_ROWS = 24
-NUM_COLUMNS = 24
-#these are the configs for the cardboard test panel
-#NUM_ROWS = 5
-#NUM_COLUMNS = 6
-
-# note: frame rate set in pattern classes
-
-#this is out lookup table that checks the run time input args and calls the appropriate pattern generator
-#at the core of it, this function just calls another pattern function, which returns a pixel array object
+# Sample call:
+# Using PI, you need sudo
+# sudo python3 pattern_master.py myconf.json
+#
+# Using visualizer,
+# python3 pattern_master.py myconf.json
+# 
+# Additional functions:
+# get_active_pattern( pattern )
+# this is out lookup table that checks the run time input args 
+# and calls the appropriate pattern generator at the core of it, 
+# this function just calls another pattern function, which 
+# returns a pixel array object
 def get_active_pattern( pattern ):
     if pattern["TYPE"] == 'test':
         active_pattern = TestPattern( my_panel.m , my_panel.n )
@@ -42,15 +33,18 @@ def get_active_pattern( pattern ):
         active_pattern = TestPattern( my_panel.m , my_panel.n )
     return active_pattern
 
+# Main
 if __name__ == '__main__':
     print('running panel_master')
+    # get json config file
     myconfig_file = sys.argv[1]
     with open(myconfig_file) as json_data_file:
         pattern_input = json.load(json_data_file)
+    # print some things
     mypanel_input = pattern_input["panel_config"]
-    print('running config: ', myconfig_file)
+    print('running config:', myconfig_file)
     run_type = mypanel_input["RUN_TYPE"]
-    print('run_typ is: ', run_type)
+    print('run_typ is:', run_type)
     m = mypanel_input["NUM_ROWS"]
     n = mypanel_input["NUM_COLUMNS"]
     pix_num = m * n
@@ -59,10 +53,12 @@ if __name__ == '__main__':
         from panel_visualizer import PanelVisualizer
         visualizer = PanelVisualizer(m, n)
     else:
-        #need to perform the import in here since we only do it if using the pi hardware
+        # need to perform the import in here since we only do it 
+        # if using the pi hardware
         from neopixel import *
-        LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
-        strip = Adafruit_NeoPixel(mypanel_input["LED_COUNT"], mypanel_input["LED_PIN"],
+        LED_STRIP = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+        LED_COUNT = mypanel_input["NUM_COLUMNS"] * mypanel_input["NUM_ROWS"]
+        strip = Adafruit_NeoPixel(LED_COUNT, mypanel_input["LED_PIN"],
                 mypanel_input["LED_FREQ_HZ"], mypanel_input["LED_DMA"], 
                 mypanel_input["LED_INVERT"], mypanel_input["LED_BRIGHTNESS"],
                 mypanel_input["LED_CHANNEL"], mypanel_input["LED_STRIP"])
