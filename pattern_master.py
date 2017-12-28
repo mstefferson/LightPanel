d9c8ee18e287ae66a2c413afda40ee0ac088aa5a
Michael Stefferson <mstefferson@gmail.com>
2017-12-28 11:40:11 -0700

    rough config working

--- pattern_master.py
+++ pattern_master.py
@@ -6,6 +6,7 @@ from functioning_patterns import *
 from inprogress_patterns import *
 from classes import Panel
 import time
+import json
 
 
 # LED strip configuration:
@@ -29,48 +30,48 @@ NUM_COLUMNS = 24
 
 #this is out lookup table that checks the run time input args and calls the appropriate pattern generator
 #at the core of it, this function just calls another pattern function, which returns a pixel array object
-def get_active_pattern( name):
-    if name == 'test':
+def get_active_pattern( pattern ):
+    if pattern["TYPE"] == 'test':
         active_pattern = TestPattern( my_panel.m , my_panel.n )
-    elif name == 'example':
+    elif pattern["TYPE"] == 'example':
         active_pattern = ExamplePattern( my_panel.m , my_panel.n )
-    elif name == 'worm':
+    elif pattern["TYPE"] == 'worm':
         active_pattern = WormPattern( my_panel.m , my_panel.n )
     else :
         print('NO VALID VISUALIZER GIVEN, USING DEFAULT')
         active_pattern = TestPattern( my_panel.m , my_panel.n )
     return active_pattern
 
-
 if __name__ == '__main__':
     print('running panel_master')
-
-    script = sys.argv[1]
-    print('running script: ', script)
-
-    run_type = sys.argv[2]
-    if run_type != "pi":
-        run_type = "vis"
+    myconfig_file = sys.argv[1]
+    with open(myconfig_file) as json_data_file:
+        pattern_input = json.load(json_data_file)
+    mypanel_input = pattern_input["panel_config"]
+    print('running config: ', myconfig_file)
+    run_type = mypanel_input["RUN_TYPE"]
+    print('run_typ is: ', run_type)
+    m = mypanel_input["NUM_ROWS"]
+    n = mypanel_input["NUM_COLUMNS"]
+    pix_num = m * n
+    if run_type == "vis":
+        print("Nothing to set-up, using visualizer") 
+        from panel_visualizer import PanelVisualizer
+        visualizer = PanelVisualizer(m, n)
     else:
         #need to perform the import in here since we only do it if using the pi hardware
         from neopixel import *
         LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
-        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ,
-            LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+        strip = Adafruit_NeoPixel(mypanel_input["LED_COUNT"], mypanel_input["LED_PIN"],
+                mypanel_input["LED_FREQ_HZ"], mypanel_input["LED_DMA"], 
+                mypanel_input["LED_INVERT"], mypanel_input["LED_BRIGHTNESS"],
+                mypanel_input["LED_CHANNEL"], mypanel_input["LED_STRIP"])
         strip.begin()
-    print('run_typ is: ', run_type)
-    m = NUM_ROWS
-    n = NUM_COLUMNS
-    pix_num = m * n
     my_panel_shapes = [ [1 for c in range(n)] for r in range(m) ]
     print('Panel . M : ', len( my_panel_shapes ), " N: ",len( my_panel_shapes[0] ))
     my_panel = Panel(m,n,pix_num,my_panel_shapes, run_type)
     # initialize pattern
-    active_pattern = get_active_pattern( script )
-
-    if run_type == "vis":
-        from panel_visualizer import PanelVisualizer
-        visualizer = PanelVisualizer(m, n)
+    active_pattern = get_active_pattern( pattern_input["pattern"] )
     #run a loop forever that just gets new pixel arrays and visualizes them
     while True:
         # update and get pixel array
