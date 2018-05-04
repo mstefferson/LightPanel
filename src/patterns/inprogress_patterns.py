eb5fdfd9c3eeb52a3b370f33f78a9210b2c3e745
dgole <dgole100@gmail.com>
2018-05-04 16:50:27 -0600

    Update inprogress_patterns.py

--- src/patterns/inprogress_patterns.py
+++ src/patterns/inprogress_patterns.py
@@ -25,7 +25,7 @@ class AudioReactiveBassPattern(PanelPattern):
         self.frame_sleep_time = 0.0
         self.pix_np = np.zeros([3,self.m,self.n])
         self.stream = micStream.Stream(fps=40,nBuffers=8)
-        self.volumeFilter   = music.ExpFilter(0.01, alpha_rise=0.05, alpha_decay=0.05)
+        self.volumeFilter   = music.ExpFilter(0.1, alpha_rise=0.1, alpha_decay=0.1)
         self.spectrumFilter = music.ExpFilter(np.zeros_like(self.stream.notes), alpha_rise=0.5, alpha_decay=0.5)
         self.colorWheel = patternHelpers.getColorWheel(3000)
         print(self.colorWheel)
@@ -46,9 +46,9 @@ class AudioReactiveBassPattern(PanelPattern):
             midIndex = self.n//2
             temp = np.sqrt(bassPower)
             temp = 20
-            self.pix_np[0,0,midIndex-temp:midIndex+temp] = 255.0 * self.colorWheel[0, frameNumEff-1000]
-            self.pix_np[1,0,midIndex-temp:midIndex+temp] = 255.0 * self.colorWheel[1, frameNumEff-1000]
-            self.pix_np[2,0,midIndex-temp:midIndex+temp] = 255.0 * self.colorWheel[2, frameNumEff-1000]            
+            #self.pix_np[0,0,midIndex-temp:midIndex+temp] = 255.0 * self.colorWheel[0, frameNumEff-1000]
+            #self.pix_np[1,0,midIndex-temp:midIndex+temp] = 255.0 * self.colorWheel[1, frameNumEff-1000]
+            #self.pix_np[2,0,midIndex-temp:midIndex+temp] = 255.0 * self.colorWheel[2, frameNumEff-1000]            
             #self.pix_np = np.clip(self.pix_np, 20, 255)
             self.pixel_arr = [ [Pixel(self.pix_np[0,j,i],self.pix_np[1,j,i],self.pix_np[2,j,i]) for i in range(self.n) ] for j in range(self.m) ]
             self.frameCount+=1
